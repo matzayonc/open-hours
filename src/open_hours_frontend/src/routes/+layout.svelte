a61bb9fe585075fc98eef80935f4d1761e1bf85b
Mateusz Zając <matzayonc@gmail.com>
2024-04-22 01:22:11 +0200

    moved to svelte 5

---
+++ src/open_hours_frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<div class="app">
+	<header>
+		<h1>Open Hours</h1>
+	</header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
