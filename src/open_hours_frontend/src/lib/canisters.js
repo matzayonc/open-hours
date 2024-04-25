625958af7367bc4ad2ab673134ea71415026f438
Mateusz Zając <matzayonc@gmail.com>
2024-04-25 21:21:01 +0200

    connected frontend

--- src/open_hours_frontend/src/lib/canisters.js
+++ src/open_hours_frontend/src/lib/canisters.js
@@ -1,12 +1,25 @@
 import { createActor, canisterId } from 'declarations/open_hours_backend';
-import { building } from '$app/environment';
+import { AuthClient } from '@dfinity/auth-client';
+import { HttpAgent } from '@dfinity/agent';
 
-function dummyActor() {
-    return new Proxy({}, { get() { throw new Error("Canister invoked while building"); } });
-}
+export const anonymousBackend = createActor(canisterId);
 
-const buildingOrTesting = building || process.env.NODE_ENV === "test";
+export const connect = async () => {
+	let authClient = await AuthClient.create();
 
-export const backend = buildingOrTesting
-    ? dummyActor()
-    : createActor(canisterId);
+	await new Promise((resolve) => {
+		authClient.login({
+			identityProvider:
+				process.env.DFX_NETWORK === 'ic'
+					? 'https://identity.ic0.app'
+					: `http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:4943/`,
+			onSuccess: resolve
+		});
+	});
+	const identity = authClient.getIdentity();
+
+	console.log(identity.getPrincipal().toText());
+
+	const agent = new HttpAgent({ identity });
+	return createActor(canisterId, { agent });
+};
--- src/open_hours_frontend/src/routes/+page.svelte
+++ src/open_hours_frontend/src/routes/+page.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
-	import { backend } from '$lib/canisters';
+	import { anonymousBackend, connect } from '$lib/canisters';
+	import { onMount } from 'svelte';
+
+	let backend: any = $state(undefined);
+	let stats: any = $state(undefined);
+
+	onMount(async () => {
+		// if (!connectedBackend) connectedBackend = await connect();
+		if (!anonymousBackend) throw new Error('No backend connection');
+		stats = await anonymousBackend.get_open_hours_stats();
+	});
+
+	function occupancy(day: string, hour: string) {
+		if (!stats) return 0;
+		const total = Number(stats[0]);
+
+		// I choose to ignore computational complexity here
+		const occupied = stats[1][days.indexOf(day)][hours.indexOf(hour)];
+		return Math.round((occupied / total) * 100);
+	}
 
 	const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 	const hours = Array.from({ length: 32 }, (_, i) =>
@@ -7,12 +26,12 @@
 	);
 
 	let marking = $state(0);
-	let chosen: string[] = $state([]);
+	let chosen: number[] = $state([]);
 
 	function startMarking(day: string, hour: string) {
 		console.log('start marking');
 
-		const targetMarked = chosen.findIndex((v) => v == day + hour) != -1;
+		const targetMarked = chosen.findIndex((v) => v == encodeSlot(day, hour)) != -1;
 		marking = targetMarked ? -1 : 1;
 
 		document.addEventListener('mouseup', () => {
@@ -25,16 +44,30 @@
 
 	function mark(day: string, hour: string) {
 		if (marking == 1) {
-			chosen = [...new Set(chosen), day + hour];
+			chosen = [...new Set(chosen), encodeSlot(day, hour)];
 		} else if (marking == -1) {
-			chosen = chosen.filter((v) => v != day + hour);
+			chosen = chosen.filter((v) => v != encodeSlot(day, hour));
 		}
+	}
+
+	function encodeSlot(day: string, hour: string) {
+		return (hours.indexOf(hour) << 3) + days.indexOf(day);
+	}
+
+	async function publish() {
+		if (!backend) backend = await connect();
+		if (!backend) return console.error('No backend connection');
+
+		const res = await backend.set_open_hours(chosen);
+		console.log(res);
+
+		stats = await anonymousBackend.get_open_hours_stats();
 	}
 </script>
 
 <header class="flex justify-between items-center">
 	<h1><b> Open Hours </b> - kiedy piwo?</h1>
-	<button class="hover:bg-cyan-400 rounded-md bg-cyan-800 p-2">Publish</button>
+	<button onclick={publish} class="hover:bg-cyan-400 rounded-md bg-cyan-800 p-2">Publish</button>
 </header>
 
 <div class="select-none grid grid-cols-6 gaps-1">
@@ -44,13 +77,14 @@
 				{day}
 			</div>
 			{#each hours as hour}
-				{#if chosen.findIndex((v) => v == day + hour) != -1}
+				{#if chosen.findIndex((v) => v == encodeSlot(day, hour)) != -1}
 					<div
 						onmousedown={() => startMarking(day, hour)}
 						onmouseenter={() => mark(day, hour)}
 						class="time marked"
 					>
 						{hour}
+						{occupancy(day, hour)}%
 					</div>
 				{:else}
 					<div
@@ -59,6 +93,7 @@
 						class="time"
 					>
 						{hour}
+						{occupancy(day, hour)}%
 					</div>
 				{/if}
 			{/each}
