64a2954b17aa74b2bb14418d0ab3c84b754299fe
Mateusz Zając <matzayonc@gmail.com>
2024-04-25 13:38:33 +0200

    the basic ui alone

--- src/open_hours_frontend/src/routes/+layout.svelte
+++ src/open_hours_frontend/src/routes/+layout.svelte
@@ -1,42 +1,7 @@
 <script>
-	import "../app.scss";
+	import '../app.scss';
 </script>
 
 <div class="app">
-	<header>
-		<h1>Open Hours</h1>
-	</header>
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<slot />
 </div>
-
-<style lang="postcss">
-	.app {
-		display: grid;
-		grid-template-rows: auto 1fr auto;
-		min-height: 100vh;
-	}
-
-	header {
-		background-color: #333;
-		color: white;
-		padding: 1em;
-	}
-
-	main {
-		padding: 1em;
-	}
-
-	footer {
-		background-color: #333;
-		color: white;
-		padding: 1em;
-		text-align: center;
-	}
-</style>
--- src/open_hours_frontend/src/routes/+page.svelte
+++ src/open_hours_frontend/src/routes/+page.svelte
@@ -1,59 +1,81 @@
-<script>
+<script lang="ts">
 	import { backend } from '$lib/canisters';
 
-	let greeting = '';
+	const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const hours = Array.from({ length: 32 }, (_, i) =>
+		i % 2 == 0 ? i / 2 + 8 + ':00' : Math.floor(i / 2) + 8 + ':30'
+	);
 
-	function onSubmit(event) {
-		const name = event.target.name.value;
-		backend.greet(name).then((response) => {
-			greeting = response;
+	let marking = $state(0);
+	let chosen: string[] = $state([]);
+
+	function startMarking(day: string, hour: string) {
+		console.log('start marking');
+
+		const targetMarked = chosen.findIndex((v) => v == day + hour) != -1;
+		marking = targetMarked ? -1 : 1;
+
+		document.addEventListener('mouseup', () => {
+			console.log('end marking');
+			marking = 0;
 		});
-		return false;
+
+		mark(day, hour);
+	}
+
+	function mark(day: string, hour: string) {
+		if (marking == 1) {
+			chosen = [...new Set(chosen), day + hour];
+		} else if (marking == -1) {
+			chosen = chosen.filter((v) => v != day + hour);
+		}
 	}
 </script>
 
-<main>
-	<form action="#" on:submit|preventDefault={onSubmit}>
-		<label for="name">Enter your name: &nbsp;</label>
-		<input id="name" alt="Name" type="text" />
-		<button type="submit">Click Me!</button>
-	</form>
-	<section id="greeting">{greeting}</section>
-</main>
+<header class="flex justify-between items-center">
+	<h1><b> Open Hours </b> - kiedy piwo?</h1>
+	<button class="hover:bg-cyan-400 rounded-md bg-cyan-800 p-2">Publish</button>
+</header>
+
+<div class="select-none grid grid-cols-6 gaps-1">
+	{#each days as day}
+		<div>
+			<div class="text-center text-md">
+				{day}
+			</div>
+			{#each hours as hour}
+				{#if chosen.findIndex((v) => v == day + hour) != -1}
+					<div
+						onmousedown={() => startMarking(day, hour)}
+						onmouseenter={() => mark(day, hour)}
+						class="time marked"
+					>
+						{hour}
+					</div>
+				{:else}
+					<div
+						onmousedown={() => startMarking(day, hour)}
+						onmouseenter={() => mark(day, hour)}
+						class="time"
+					>
+						{hour}
+					</div>
+				{/if}
+			{/each}
+		</div>
+	{/each}
+</div>
 
 <style lang="postcss">
-	main {
-		display: grid;
-		place-items: center;
-		height: 100vh;
+	header {
+		@apply bg-gray-800 text-white p-4;
 	}
 
-	form {
-		display: grid;
-		grid-template-columns: auto auto;
-		align-items: center;
-		gap: 1em;
+	.time {
+		@apply select-none transition duration-200 hover:bg-gray-200 text-sm rounded-lg bg-cyan-200 m-1 border border-cyan-400 p-1 text-center;
+
+		&.marked {
+			@apply bg-cyan-400;
+		}
 	}
-
-	label {
-		text-align: right;
-	}
-
-	input {
-		padding: 0.5em;
-	}
-
-
-	button {
-		@apply font-bold py-2 px-4 rounded;
-		background-color: #333;
-		color: white;
-	}
-
-	#greeting {
-		margin-top: 1em;
-	}
-
 </style>
-
-
